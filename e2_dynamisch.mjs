20390646420a75866c48d4ab5fccc1ea041a0a6c
Hana-amr <hna_amr@outlook.com>
2024-09-25 11:49:35 +0200

    Update e2_dynamisch.mjs

--- e2_dynamisch.mjs
+++ e2_dynamisch.mjs
@@ -5,11 +5,10 @@ const userInput = readline.createInterface({input, output});
 // Einde van userInput voorbereiden
 
 
-
 // Voeg hier je eigen code in
-
-
-
+let name = 'Hana'
+let age = '17'
+let favoriteGame = 'NeedForSpeed'
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
 
 process.exit();
