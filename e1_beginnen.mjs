0e252f6d1b910d7d4fe1080134ed3a8fa3a0b440
Hana-amr <hna_amr@outlook.com>
2024-09-25 11:31:44 +0200

    Update e1_beginnen.mjs

--- e1_beginnen.mjs
+++ e1_beginnen.mjs
@@ -1,5 +1,4 @@
-// Voeg hier je eigen code in
-
-
-
+let name = 'Hana';
+let age = '17';
+let favoriteGame = 'NeedForSpeed'
 console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
